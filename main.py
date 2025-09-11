77ddf5152fcdf21201d40be0321e151f9b71fb1e
Matt Sky L.Razonable <razonable.ml@obmontessori.edu.ph>
2025-09-11 21:09:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 from pyscript import display
 
+menu = {
+    "Crushy Dynamite Roll": 400.00,
+    "Curry Ramen": 500.00,
+    "Yakitori": 255.00,
+    "Tempura": 385.00,
+    "Donbori": 600.00
+}
+
 restaurant_name = "Oishii Nook"
 owner_name = "ona Matt Sky-san"
 year_since = 2025
 
-menu = [
-    ("Crushy Dynamite Roll", "sushi roll packed with shrimp, mushroom, avocado, cucumber and spicy kick", 400.00),
-    ("Curry Ramen", "ramen with curry sauce and chicken teriyaki", 500.00),
-    ("Yakitori", "grilled chicken skewers", 255.00),
-    ("Tempura", "battered and deep fried seafood and vegetables", 385.00),
-    ("Donbori (choice of Gyudon or Oyakodon)", "rice bowls topped with beef (gyudon) or chicken and egg (oyakodon)", 600.00)
-]
-
-display(restaurant_name, target="name1")  
-display(f"Owner: {owner_name}", target="Owner")
+display(restaurant_name, target="One")
+display(f"{owner_name}", target="Owner")
 display(f"Since {year_since}", target="Since")
+display("Menu and Pricelist", target="heading1")
 
-for i, (name, desc, price) in enumerate(menu, start=1):
-    display(name, target=f"prod{i}")
-    display(desc, target=f"desc{i}")
-    display(f"PhP {price:,.2f}", target=f"price{i}")
+
+display("Crushy Dynamite Roll", target="prod1")
+display(f"₱{menu['Crushy Dynamite Roll']:.2f}", target="price1")
+
+display("Curry Ramen", target="prod2")
+display(f"₱{menu['Curry Ramen']:.2f}", target="price2")
+
+display("Yakitori", target="prod3")
+display(f"₱{menu['Yakitori']:.2f}", target="price3")
+
+display("Tempura", target="prod4")
+display(f"₱{menu['Tempura']:.2f}", target="price4")
+
+display("Donbori", target="prod5")
+display(f"₱{menu['Donbori']:.2f}", target="price5")
